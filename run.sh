7690f8e6aae18030cd921e15f55996f702cafb2b
Edmund Luong <edmundvmluong@gmail.com>
2015-03-28 09:32:48 -0400

    Added maven install-file to run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+mvn install:install-file -Dfile=src/main/java/resources/jar/LeapJava.jar -DgroupId=com.leapmotion.leap -DartifactId=leapMotion -Dversion=2.2.4 -Dpackaging=jar
 mvn clean
 mvn compile
 mvn package
