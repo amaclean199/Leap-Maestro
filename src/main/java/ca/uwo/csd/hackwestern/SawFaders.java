4461ec4b88718506375da0f38b2b98f6b0de6d42
Alex MacLean <maclean199@gmail.com>
2015-03-29 01:37:19 -0400

    old master

--- src/main/java/ca/uwo/csd/hackwestern/SawFaders.java
+++ src/main/java/ca/uwo/csd/hackwestern/SawFaders.java
@@ -1,19 +1,17 @@
 package src.main.java.ca.uwo.csd.hackwestern;
 
-import java.awt.Component;
-import java.awt.GridLayout;
+import java.awt.*;
 
 import javax.swing.JApplet;
 import javax.swing.JPanel;
 
-import com.jsyn.JSyn;
-import com.jsyn.Synthesizer;
+import com.jsyn.*;
 import com.jsyn.swing.ExponentialRangeModel;
 import com.jsyn.swing.PortControllerFactory;
 import com.jsyn.swing.PortModelFactory;
 import com.jsyn.swing.RotaryTextController;
+import com.jsyn.unitgen.AsymptoticRamp;
 import com.jsyn.unitgen.LineOut;
-import com.jsyn.unitgen.LinearRamp;
 import com.jsyn.unitgen.SawtoothOscillatorBL;
 import com.jsyn.unitgen.UnitOscillator;
 
@@ -28,44 +26,43 @@ public class SawFaders extends JApplet
 	private static final long serialVersionUID = -2704222221111608377L;
 	private Synthesizer synth;
 	private UnitOscillator osc;
-	private LinearRamp lag;
+	private AsymptoticRamp lag;
 	private LineOut lineOut;
 	private double customFrq;
-	//private PortControllerFactory pCF;
 	private Component x;
 	private ExponentialRangeModel amplitudeModel;
-	
+
 	public void init()
 	{
 		synth = JSyn.createSynthesizer();
-		
 		// Add a tone generator.
 		synth.add( osc = new SawtoothOscillatorBL() );
 		// Add a lag to smooth out amplitude changes and avoid pops.
-		synth.add( lag = new LinearRamp() );
+		synth.add( lag = new AsymptoticRamp() );
+		double hlife = 0.01 + (Math.random() * 0.9);
+		lag.halfLife.set( hlife );
 		// Add an output mixer.
 		synth.add( lineOut = new LineOut() );
 		// Connect the oscillator to the output.
 		osc.output.connect( 0, lineOut.input, 0 );
-		
 		// Set the minimum, current and maximum values for the port.
 		lag.output.connect( osc.amplitude );
 		lag.input.setup( 0.0, 0.5, 1.0 );
-		lag.time.set(  0.2 );
 
 		// Arrange the faders in a stack.
 		setLayout( new GridLayout( 0, 1 ) );
-
+	
 		amplitudeModel = PortModelFactory.createExponentialModel( lag.input );
 		RotaryTextController knob = new RotaryTextController( amplitudeModel, 5 );
 		JPanel knobPanel = new JPanel();
 		knobPanel.add( knob );
 		add( knobPanel );
 
-		osc.frequency.setup( 50.0, 300.0, 10000.0 );
-		osc.amplitude.setup(0, 0.5, 1);
+		osc.frequency.setup( 261, 300.0, 2000.0 );
+
 		x = PortControllerFactory.createExponentialPortSlider( osc.frequency );
 		add( x );
+		osc.amplitude.setup(0, 0.5, 1);
 		validate();
 	}
 	
@@ -77,9 +74,10 @@ public class SawFaders extends JApplet
 	public void updatePosition()
 	{
 		remove (x);
-		osc.frequency.setup(50.0, getFrq(), 1000.0);
+		osc.frequency.setup(261, getFrq(), 2000.0);
 		x = PortControllerFactory.createExponentialPortSlider( osc.frequency );
 		add( x );
+		osc.output.connect( 0, lineOut.input, 0 );
 		validate();
 	}
 	
@@ -87,8 +85,8 @@ public class SawFaders extends JApplet
 	{
 		// Start synthesizer using default stereo output at 44100 Hz.
 		synth.start();
-		
-		// We only need to start the LineOut. It will pull data from the oscillator.
+		// We only need to start the LineOut. It will pull data from the
+		// oscillator.
 		lineOut.start();
 	}
 
